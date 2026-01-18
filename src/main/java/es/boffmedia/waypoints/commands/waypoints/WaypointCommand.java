ff8f1920d568262cf533c849305e45c57bd7d472
Luis Carballo <luisca343@gmail.com>
2026-01-18 17:45:11 +0100

    Revert "Add toggleable coordinates HUD feature"
    This reverts commit 2473e85c3fcc928f53f36aefc699a140174ba82b.

--- src/main/java/es/boffmedia/waypoints/commands/waypoints/WaypointCommand.java
+++ src/main/java/es/boffmedia/waypoints/commands/waypoints/WaypointCommand.java
@@ -28,7 +28,6 @@ public class WaypointCommand extends AbstractPlayerCommand {
         this.config = config;
         addSubCommand(new AddWaypointCommand());
         addSubCommand(new RemoveWaypointCommand());
-        addSubCommand(new ToggleHUDCommand());
         addAliases("wp", "waypoints");
         setPermissionGroup(GameMode.Adventure);
     }
